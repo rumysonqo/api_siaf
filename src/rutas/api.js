0d372a87c43c3b745abd05f854d5b49610bbc9a5
abel laurente <abellaurente@hotmail.com>
2022-05-06 16:42:14 -0500

    con generica

--- src/rutas/api.js
+++ src/rutas/api.js
@@ -23,6 +23,16 @@ rutas.get('/api/fuentes',(req,res)=>{
     });
 });
 
+rutas.get('/api/genericas',(req,res)=>{
+    con.query('select distinct(cod_generica) as codigo, generica from rep_siaf',(err,rows,fields)=>{
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
 rutas.get('/api/rep_prog_fte/:fte',(req,res)=>{
     const { fte } = req.params;
     con.query('select programa,fuente,sum(pia) as pia,sum(pim) as pim,sum(certificado) as certificado,sum(devengado) as devengado, sum(pim)-sum(devengado) as saldo, (sum(devengado)/sum(pim))*100 as avance from rep_siaf where cod_fuente= ? group by programa,fuente order by programa,fuente',[fte],(err,rows,fields)=>{
@@ -37,6 +47,18 @@ rutas.get('/api/rep_prog_fte/:fte',(req,res)=>{
 
 rutas.get('/api/totales',(req,res)=>{
     con.query('select sum(pia) as pia,sum(pim) as pim,sum(certificado) as certificado,sum(devengado) as devengado, sum(pim)-sum(devengado) as saldo, (sum(devengado)/sum(pim))*100 as avance from rep_siaf ',(err,rows,fields)=>{
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+//totales por generica
+rutas.get('/api/totales_gen/:gen',(req,res)=>{
+    const { gen } = req.params;
+    con.query('select sum(pia) as pia,sum(pim) as pim,sum(certificado) as certificado,sum(devengado) as devengado, sum(pim)-sum(devengado) as saldo, (sum(devengado)/sum(pim))*100 as avance from rep_siaf where cod_generica = ?',[gen],(err,rows,fields)=>{
         if (!err) {
             res.json(rows);
         } else {
@@ -94,6 +116,19 @@ rutas.get('/api/ejec_crd',(req,res)=>{
         }
     });
 });
+
+rutas.get('/api/ejec_fte_gen/:fte/:gen',(req,res)=>{
+    const { fte, gen } = req.params;
+    con.query('select sum(pia) as pia,sum(pim) as pim,sum(certificado) as certificado,sum(devengado) as devengado, sum(pim)-sum(devengado) as saldo, (sum(devengado)/sum(pim))*100 as avance from rep_siaf where cod_fuente= ? and cod_generica = ?',[fte,gen],(err,rows,fields)=>{
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
 
 
 module.exports=rutas;
