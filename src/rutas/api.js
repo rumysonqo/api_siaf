2395f433129d6f2ca1aced4e3d5e731ed9a231eb
abel laurente <abellaurente@hotmail.com>
2022-08-01 09:22:13 -0500

    ultimo

--- src/rutas/api.js
+++ src/rutas/api.js
@@ -33,6 +33,16 @@ rutas.get('/api/genericas',(req,res)=>{
     });
 });
 
+rutas.get('/api/programas',(req,res)=>{
+    con.query('select distinct(cod_programa) as codigo, programa from rep_siaf',(err,rows,fields)=>{
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
 rutas.get('/api/rep_prog_fte/:fte',(req,res)=>{
     const { fte } = req.params;
     con.query('select programa,fuente,sum(pia) as pia,sum(pim) as pim,sum(certificado) as certificado,sum(devengado) as devengado, sum(pim)-sum(devengado) as saldo, (sum(devengado)/sum(pim))*100 as avance from rep_siaf where cod_fuente= ? group by programa,fuente order by programa,fuente',[fte],(err,rows,fields)=>{
